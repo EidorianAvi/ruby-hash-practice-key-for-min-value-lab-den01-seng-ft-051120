9150af5d8e372b54182b7b33cc078c34d6632a33
Rene Avila <reneavila1993@gmail.com>
2020-05-16 18:06:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,9 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+  low = 0 
   name_hash.map do |key, value|
-    value.sort.first
+    if value > low 
+      
   end
 end
